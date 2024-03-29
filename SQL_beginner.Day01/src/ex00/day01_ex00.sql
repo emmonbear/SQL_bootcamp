8f22ebb063095f96eb89ae2d6639dfe980268141
emmonbear <moskaleviluak@icloud.com>
2024-03-29 22:50:52 +0500

    feat[ex00]: add sql statement

---
+++ SQL_beginner.Day01/src/ex00/day01_ex00.sql
@@ -0,0 +1,12 @@
+SELECT
+      p.id AS object_id,
+      p.name AS object_name
+FROM person AS p
+UNION ALL
+SELECT
+      m.id AS object_id,
+      m.pizza_name AS object_name
+FROM menu AS m
+ORDER BY 
+      1 ASC,
+      2 ASC 
