526d5018f11117ab0c2f67c436f3ad6779f81d71
emmonbear <moskaleviluak@icloud.com>
2024-04-04 11:08:26 +0500

    feat[ex07]: add sql statement

---
+++ SQL_beginner.Day01/src/ex07/day01_ex07.sql
@@ -0,0 +1,8 @@
+SELECT
+      order_date,
+      p.name || ' (age:' || p.age || ')' AS person_information
+  FROM person_order AS po
+  JOIN person AS p ON p.id =  po.person_id
+ ORDER BY
+      order_date ASC,
+      person_information ASC;
