d2eec0a2599c763f17a3304e0882d421127d3e32
emmonbear <moskaleviluak@icloud.com>
2024-04-04 20:40:14 +0500

    fix[ex04]: modify sql statement

--- SQL_beginner.Day01/src/ex04/day01_ex04.sql
+++ SQL_beginner.Day01/src/ex04/day01_ex04.sql
@@ -1,14 +1,12 @@
 SELECT
-    po.person_id,
-    po.order_date
+    po.person_id
 FROM
     person_order AS po
 WHERE
     po.order_date = '2022-01-07'
 EXCEPT ALL
 SELECT
-    pv.person_id,
-    pv.visit_date
+    pv.person_id
 FROM
     person_visits AS pv
 WHERE
