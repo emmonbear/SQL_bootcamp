350de693904389e0bfdd640bd7c38776a33d570e
emmonbear <moskaleviluak@icloud.com>
2024-04-04 13:26:37 +0500

    feat[ex08]: add sql statement

---
+++ SQL_beginner.Day01/src/ex08/day01_ex08.sql
@@ -0,0 +1,15 @@
+SELECT
+      order_date,
+      p.name || ' (age:' || p.age || ')' AS person_information
+  FROM (
+      SELECT 
+            po.person_id AS id,
+            po.order_date
+        FROM person_order AS po
+  )
+NATURAL JOIN person AS p
+ ORDER BY
+      order_date,
+      person_information;
+
+      
