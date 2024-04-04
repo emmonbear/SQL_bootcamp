4fc593c056a1b3691aee3575402d145b6e87d247
emmonbear <moskaleviluak@icloud.com>
2024-04-04 15:34:20 +0500

    feat[ex10]: add sql statement

---
+++ SQL_beginner.Day01/src/ex10/day01_ex10.sql
@@ -0,0 +1,10 @@
+SELECT
+      p.name       AS person_name,
+      m.pizza_name AS pizza_name,
+      pp.name      AS pizzeria_name
+  FROM person_order AS po
+JOIN person AS p ON p.id = po.person_id
+JOIN menu AS m ON m.id = po.menu_id
+JOIN pizzeria AS pp ON pp.id = m.pizzeria_id
+ORDER BY person_name, pizza_name, pizzeria_name;
+
