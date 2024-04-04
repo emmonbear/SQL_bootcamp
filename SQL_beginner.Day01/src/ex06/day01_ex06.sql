bcb85ee534c6168edf919634362846f9e0a21b5b
emmonbear <moskaleviluak@icloud.com>
2024-04-04 10:43:55 +0500

    feat[ex06]: add sql statement

---
+++ SQL_beginner.Day01/src/ex06/day01_ex06.sql
@@ -0,0 +1,21 @@
+SELECT
+      action_date,
+      name AS person_name
+  FROM (
+      SELECT
+            po.order_date AS action_date,
+            po.person_id AS person_id
+        FROM person_order AS po
+   INTERSECT ALL
+      SELECT
+            pv.visit_date AS action_date,
+            pv.person_id AS person_id
+        FROM person_visits AS pv
+       ORDER BY
+            action_date ASC,
+            person_id DESC
+) AS p
+  JOIN person AS pp ON p.person_id = pp.id
+ ORDER BY
+      action_date ASC,
+      person_name DESC
