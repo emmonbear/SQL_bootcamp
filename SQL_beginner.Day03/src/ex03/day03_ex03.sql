97d6ab2f63b6f0af8c8a8e357b85ff24cc6a8143
emmonbear <moskaleviluak@icloud.com>
2024-04-06 16:37:16 +0500

    feat(day03_ex03): add sql statement

---
+++ SQL_beginner.Day03/src/ex03/day03_ex03.sql
@@ -0,0 +1,22 @@
+WITH a AS (
+            SELECT
+                  p.gender,
+                  pp.name
+            FROM
+                  person_visits AS pv
+                  JOIN person AS p ON p.id = pv.person_id
+                  JOIN pizzeria AS pp ON pp.id = pv.pizzeria_id),
+      male AS (SELECT a.name FROM a WHERE a.gender = 'male'),
+      female AS (SELECT a.name FROM a WHERE a.gender = 'female') 
+(
+      SELECT * FROM male
+      EXCEPT ALL
+      SELECT * FROM female
+)
+UNION ALL
+(
+      SELECT * FROM female
+      EXCEPT ALL
+      SELECT * FROM male
+)
+ORDER BY 1;
