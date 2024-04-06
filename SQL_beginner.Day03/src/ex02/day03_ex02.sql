daae69a0c878e2f24b399b1e4b88a264a3ca88fb
emmonbear <moskaleviluak@icloud.com>
2024-04-06 15:51:51 +0500

    feat(day03_ex02): add sql statement

---
+++ SQL_beginner.Day03/src/ex02/day03_ex02.sql
@@ -0,0 +1,15 @@
+WITH a AS (
+            SELECT m.id AS menu_id
+            FROM menu AS m
+            EXCEPT
+            SELECT po.menu_id
+            FROM person_order AS po)
+SELECT
+      m.pizza_name AS pizza_name,
+      m.price      AS price,
+      p.name       AS pizzeria_name
+FROM
+      a
+      JOIN menu AS m ON m.id = a.menu_id
+      JOIN pizzeria AS p ON p.id = m.pizzeria_id
+ORDER BY 1, 2;
