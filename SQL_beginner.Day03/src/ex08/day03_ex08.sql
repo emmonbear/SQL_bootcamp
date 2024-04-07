5648b218d2d5eb1dc780f3b59598014e45d89e1a
emmonbear <moskaleviluak@icloud.com>
2024-04-07 17:58:22 +0500

    feat(day03_ex08): add sql statement

---
+++ SQL_beginner.Day03/src/ex08/day03_ex08.sql
@@ -0,0 +1,8 @@
+INSERT INTO
+      menu (id, pizzeria_id, pizza_name, price)
+VALUES (
+      (SELECT (MAX(m.id) + 1) FROM menu AS m),
+      (SELECT p.id FROM pizzeria AS p WHERE p.name = 'Dominos'),
+      'sicilian pizza',
+      900
+);
