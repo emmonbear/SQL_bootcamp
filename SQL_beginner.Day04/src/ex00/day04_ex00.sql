bc5ea8d31371c22424d7076e06e5982a9c0fb48e
emmonbear <moskaleviluak@icloud.com>
2024-04-08 21:34:10 +0500

    fix(day04_ex05): modify sql statement

--- SQL_beginner.Day04/src/ex00/day04_ex00.sql
+++ SQL_beginner.Day04/src/ex00/day04_ex00.sql
@@ -1,13 +1,9 @@
 CREATE VIEW v_persons_female AS
 SELECT *
   FROM person AS p
- WHERE p.gender = 'female'
+ WHERE p.gender = 'female';
 
 CREATE VIEW v_persons_male AS
 SELECT *
   FROM person AS p
- WHERE p.gender = 'male'
-
-SELECT * FROM v_persons_male
-
-DROP VIEW v_persons_female
+ WHERE p.gender = 'male';
