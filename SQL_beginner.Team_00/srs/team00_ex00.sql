9d737b208e568b3421097bd52b54cf775d1cd774
emmonbear <moskaleviluak@icloud.com>
2024-04-10 10:00:07 +0500

    feat: add recursive cte

--- SQL_beginner.Team_00/srs/team00_ex00.sql
+++ SQL_beginner.Team_00/srs/team00_ex00.sql
@@ -1,3 +1,4 @@
+-- Active: 1711731107020@@127.0.0.1@5432@team00
 CREATE TABLE paths (
        point_1 VARCHAR(10) NOT NULL,
        point_2 VARCHAR(10) NOT NULL,
@@ -17,3 +18,21 @@ VALUES ('a', 'b', 10),
        ('d', 'a', 20),
        ('d', 'b', 25),
        ('d', 'c', 30);
+
+WITH RECURSIVE tsp (tour, total_cost, last_point) AS (
+     SELECT p.point_1::VARCHAR,
+            0,
+            p.point_1
+       FROM paths AS p
+      WHERE p.point_1 = 'a'
+      UNION
+     SELECT t.tour || p.point_2,
+            t.total_cost + p.price,
+            p.point_2
+       FROM tsp AS t
+            JOIN paths AS p
+            ON t.last_point = p.point_1
+            AND NOT (STRPOS(t.tour, p.point_2) > 0)
+)
+SELECT *
+  FROM tsp
