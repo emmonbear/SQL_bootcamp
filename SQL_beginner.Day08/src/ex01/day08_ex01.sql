de8ec4017e80caa928a7ff1a7abd9546e3da681a
emmonbear <moskaleviluak@icloud.com>
2024-04-14 18:03:22 +0500

    feat(day08_ex01): add sql statement

---
+++ SQL_beginner.Day08/src/ex01/day08_ex01.sql
@@ -0,0 +1,24 @@
+--Session 1
+BEGIN;
+--Session 2
+BEGIN;
+
+--Session 1
+SELECT p.rating FROM pizzeria AS p WHERE p.name = 'Pizza Hut';
+--Session 2
+SELECT p.rating FROM pizzeria AS p WHERE p.name = 'Pizza Hut';
+
+--Session 1
+UPDATE pizzeria SET rating = 4.0 WHERE name = 'Pizza Hut';
+--Session 2
+UPDATE pizzeria SET rating = 3.6 WHERE name = 'Pizza Hut';
+
+--Session 1
+COMMIT;
+--Session 2
+COMMIT;
+
+--Session 1
+SELECT p.rating FROM pizzeria AS p WHERE p.name = 'Pizza Hut';
+--Session 2
+SELECT p.rating FROM pizzeria AS p WHERE p.name = 'Pizza Hut';
