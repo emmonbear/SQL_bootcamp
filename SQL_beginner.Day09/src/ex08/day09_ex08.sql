c71702df5c60be1ba0bb24f5e7b4562bd5dfc4ce
emmonbear <moskaleviluak@icloud.com>
2024-04-22 21:22:51 +0500

    feat(day09_ex08): add sql statement

---
+++ SQL_beginner.Day09/src/ex08/day09_ex08.sql
@@ -0,0 +1,17 @@
+CREATE OR REPLACE FUNCTION fnc_fibonacci(IN pstop INTEGER DEFAULT 10)
+RETURNS TABLE (a INTEGER) AS
+        $$
+           WITH RECURSIVE f(a, b) AS
+                (SELECT 0 AS a, 1 AS b
+                UNION ALL
+                SELECT b, a + b
+                FROM f
+                WHERE b < pstop
+                )
+         SELECT a
+           FROM f;
+        $$ LANGUAGE SQL;
+
+select * from fnc_fibonacci(1000);
+
+select * from fnc_fibonacci();
