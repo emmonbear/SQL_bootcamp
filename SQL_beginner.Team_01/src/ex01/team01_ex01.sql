f2c808344446c58ae3b571c4250722b0bd9bb2b1
emmonbear <moskaleviluak@icloud.com>
2024-04-25 22:06:07 +0500

    feat(team01_ex01: add sql statement

--- SQL_beginner.Team_01/src/ex01/team01_ex01.sql
+++ SQL_beginner.Team_01/src/ex01/team01_ex01.sql
@@ -1,3 +1,6 @@
+insert into currency values (100, 'EUR', 0.85, '2022-01-01 13:29');
+insert into currency values (100, 'EUR', 0.79, '2022-01-08 13:29');
+
   WITH currency_rate AS
        (SELECT b.user_id,
                b.money,
